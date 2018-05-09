db9b4d4a39e117640319f1e320ec3edf1cb0c5a7
BdonGordon <bgordon@linamar.com>
2018-05-09 11:00:15 -0400

    Brandon Gordon 5/9/2018 @ 11:00:
    - Added AppContainer and CoreLayout

--- ReactNative/test-app/App.tsx
+++ ReactNative/test-app/App.tsx
@@ -1,13 +1,12 @@
 import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AppContainer from './src/containers/AppContainer';
 
 export default class App extends React.Component<{}> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.ts to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <View style={styles.container}>
+            <AppContainer />
       </View>
     );
   }
---
+++ ReactNative/test-app/src/containers/AppContainer.tsx
@@ -0,0 +1,15 @@
+﻿import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import CoreLayout from '../layouts/CoreLayout';
+
+class AppContainer extends React.Component<{}> {
+    render() {
+        return (
+            <View>
+                <CoreLayout />
+            </View>
+        );
+    }
+}
+
+export default AppContainer;
