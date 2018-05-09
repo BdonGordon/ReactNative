cabee007f95c60c5de41698079513c9de1cda3da
BdonGordon <bgordon@linamar.com>
2018-05-09 13:12:41 -0400

    5/9/2018 @ 13:10:
    - Added NavigationDrawer components (nothing in it yet)

---
+++ ReactNative/test-app/src/components/NavigationDrawer/component/NavigationDrawer.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class NavigationDrawer extends React.Component<{}> {
+    
+}
+
+export default NavigationDrawer;
